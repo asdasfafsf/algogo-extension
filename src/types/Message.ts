577088d03da39bdd18d8d5860ea72f6fa662d1d6
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2025-03-07 20:07:44 +0900

    feat: 유형별로 타입 추가

--- src/types/Message.ts
+++ src/types/Message.ts
@@ -4,15 +4,47 @@ export type MessageType = typeof MessageType[keyof typeof MessageType];
 
 
 export interface MessagePayloadMap {
-    [MessageType.SUBMIT]: Submit;
-    [MessageType.CHECK_LOGIN]: {source: Source};
-    [MessageType.RESULT]: {source: Source};
-    [MessageType.EXECUTE]: Submit;
-    [MessageType.REQUEST_PROGRESS]: {tabId: Number, source: Source};
-    [MessageType.RESPONSE_PROGRESS]: {tabId: Number, source: Source};
+    [MessageType.WEB_TO_CONTENT_SCRIPT_SUBMIT]: Submit;
+    [MessageType.WEB_TO_CONTENT_SCRIPT_PROGRESS]: {tabId: Number, source: Source};
+    
+    [MessageType.CONTENT_SCRIPT_TO_BACKGROUND_SUBMIT]: Submit;
+    [MessageType.CONTENT_SCRIPT_TO_BACKGROUND_PROGRESS]: {tabId: Number, source: Source};
+    
+    [MessageType.BACKGROUND_TO_NEW_CONTENT_SCRIPT_CHECK_LOGIN]: {source: Source};
+    [MessageType.BACKGROUND_TO_NEW_CONTENT_SCRIPT_SUBMIT]: Submit;
+    [MessageType.BACKGROUND_TO_NEW_CONTENT_SCRIPT_RESULT]: {source: Source};
+    [MessageType.BACKGROUND_TO_NEW_CONTENT_SCRIPT_PROGRESS]: {tabId: Number, source: Source};
+   
+    [MessageType.NEW_CONTENT_SCRIPT_TO_BACKGROUND_CHECK_LOGIN]: {source: Source};
+    [MessageType.NEW_CONTENT_SCRIPT_TO_BACKGROUND_SUBMIT]: Submit;
+    [MessageType.NEW_CONTENT_SCRIPT_TO_BACKGROUND_PROGRESS]: {tabId: Number, source: Source};
+    [MessageType.NEW_CONTENT_SCRIPT_TO_BACKGROUND_RESULT]: {source: Source};
 }
+
+export interface MessageResponseMap {
+    [MessageType.WEB_TO_CONTENT_SCRIPT_SUBMIT]: Submit;
+    [MessageType.WEB_TO_CONTENT_SCRIPT_PROGRESS]: {tabId: Number, source: Source};
+    
+    [MessageType.CONTENT_SCRIPT_TO_BACKGROUND_SUBMIT]: Submit;
+    [MessageType.CONTENT_SCRIPT_TO_BACKGROUND_PROGRESS]: {tabId: Number, source: Source};
+    
+    [MessageType.BACKGROUND_TO_NEW_CONTENT_SCRIPT_CHECK_LOGIN]: {source: Source};
+    [MessageType.BACKGROUND_TO_NEW_CONTENT_SCRIPT_SUBMIT]: Submit;
+    [MessageType.BACKGROUND_TO_NEW_CONTENT_SCRIPT_RESULT]: {source: Source};
+    [MessageType.BACKGROUND_TO_NEW_CONTENT_SCRIPT_PROGRESS]: {tabId: Number, source: Source};
+   
+    [MessageType.NEW_CONTENT_SCRIPT_TO_BACKGROUND_CHECK_LOGIN]: {source: Source};
+    [MessageType.NEW_CONTENT_SCRIPT_TO_BACKGROUND_SUBMIT]: Submit;
+    [MessageType.NEW_CONTENT_SCRIPT_TO_BACKGROUND_PROGRESS]: {tabId: Number, source: Source};
+    [MessageType.NEW_CONTENT_SCRIPT_TO_BACKGROUND_RESULT]: {source: Source};
+}   
 
 export type Message<T extends MessageType> = {
     type: T;
     data: MessagePayloadMap[T];
+}
+
+export type Response<T extends MessageType> = {
+    type: T;
+    data: MessageResponseMap[T];
 }
